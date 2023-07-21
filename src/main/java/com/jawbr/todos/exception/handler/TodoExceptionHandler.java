d364b55ed9f6cd00de1b52b48521cdac88d6be4c
Bradley Jawwad <bradleyjawwad@hotmail.com>
2023-07-20 22:15:35 -0300

    Add Tratamento de Exception
    Adicionado tratamento da exceção MethodArgumentNotValidException para as endpoints que usam @Valid

--- src/main/java/com/jawbr/todos/exception/handler/TodoExceptionHandler.java
+++ src/main/java/com/jawbr/todos/exception/handler/TodoExceptionHandler.java
@@ -4,8 +4,12 @@ import com.jawbr.todos.exception.TodoNotFoundException;
 import com.jawbr.todos.exception.errorResponse.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
 
 @ControllerAdvice
 public class TodoExceptionHandler {
@@ -14,5 +18,13 @@ public class TodoExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(TodoNotFoundException exc) {
         ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exc.getMessage(), System.currentTimeMillis());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exc) {
+        BindingResult result = exc.getBindingResult();
+        String message = Objects.requireNonNull(result.getFieldError()).getDefaultMessage();
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message, System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
