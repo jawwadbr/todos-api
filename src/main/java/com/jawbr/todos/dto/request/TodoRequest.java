9ef3a33a54a038a157114d42ad8290df18105977
Bradley Jawwad <bradleyjawwad@hotmail.com>
2023-07-21 06:20:50 -0300

    Update TodoRequest.java
    Add lombok builder to the DTO request

--- src/main/java/com/jawbr/todos/dto/request/TodoRequest.java
+++ src/main/java/com/jawbr/todos/dto/request/TodoRequest.java
@@ -1,7 +1,9 @@
 package com.jawbr.todos.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record TodoRequest(
         @NotBlank(message = "Nome da tarefa não pode estar vazia!") String nome,
         @NotBlank(message = "Descrição da tarefa não pode estar vazia!") String descricao,
